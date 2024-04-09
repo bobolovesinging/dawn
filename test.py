0df21bb6e986d47b2c06d5c1391016f2817bc561
bobolovesinging <166495929+bobolovesinging@users.noreply.github.com>
2024-04-09 17:12:22 +0800

    Update and rename hello.py to test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+
+class Dawn(nn.Module):
+    def __init__(self):
+        super(Dawn, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64 * 4 * 4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+   
